5eae4957a4c789d3af95a47f47a57ee4d3a8dd45
t_dsgomes <deivison.gomes@avanz.com.br>
2024-05-17 11:13:44 -0300

    fix

--- src/server.js
+++ src/server.js
@@ -20,27 +20,108 @@ app.post("/buscar-perfil", async (req, res) => {
   try {
     console.log(`Iniciando a captura do perfil ${username}`);
     const tmpDir = tmp.dirSync();
-    
+
     const browser = await puppeteer.launch({
-      cacheDir: '/workspace/.cache/puppeteer/chrome/linux-125.0.6422.60/chrome-linux64/chrome',
-      args: ['--no-sandbox'],
-      userDataDir: tmpDir.name
+      headless: false,
+      args: ["--no-sandbox", "--start-fullscreen"],
+      userDataDir: tmpDir.name,
     });
-    
+
     const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.999 Safari/537.36');
-    await page.goto(`https://www.instagram.com/${username}/`);
+    console.log("Nova página criada.");
+
     await page.setViewport({ width: 375, height: 812 });
-    await delay(4000);
-    const screenshotBase64 = await page.screenshot({ encoding: "base64" });
-    capturedImages[username] = Buffer.from(screenshotBase64, "base64");
+    console.log("Viewport configurado.");
+
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.999 Safari/537.36"
+    );
+    console.log("User agent configurado.");
+
+    const cookiesPath = 'cookies.json';
+    let loggedIn = false;
+
+    // Verificar se o arquivo de cookies existe
+    if (fs.existsSync(cookiesPath)) {
+      const cookies = JSON.parse(fs.readFileSync(cookiesPath, 'utf8'));
+      if (cookies.length > 0) {
+        await page.setCookie(...cookies);
+        console.log("Cookies carregados.");
+        
+        // Verificar se o login ainda é válido
+        await page.goto(`https://www.instagram.com/${username}/`);
+        await delay(5000); // Aguarde o carregamento da página
+
+        const loginCheck = await page.$('input[name="username"]');
+        if (!loginCheck) {
+          loggedIn = true;
+        }
+      }
+    }
+    if (!loggedIn) {
+      console.log("Realizando login.");
+      await page.goto("https://www.instagram.com/accounts/login/");
+      await page.waitForSelector('input[name="username"]', { visible: true });
+      console.log("Campo de usuário encontrado.");
+      await delay(500);
+      await page.type('input[name="username"]', process.env.INSTAGRAM_USERNAME, delay(50));
+      console.log("Usuário digitado.");
+
+      await delay(500);
+      await page.type('input[name="password"]', process.env.INSTAGRAM_PASSWORD, delay(50));
+      console.log("Senha digitada.");
+      await delay(500);
+
+      await page.click('button[type="submit"]');
+      console.log("Botão de login clicado.");
+      
+      await page.waitForNavigation();
+      console.log("Navegação após login.");
+
+      // Verificar se o login foi bem-sucedido
+      const loginFailed = await page.$('div[role="alert"]');
+      if (loginFailed) {
+        throw new Error("Falha no login do Instagram.");
+      }
+      console.log("Login bem-sucedido.");
+
+      // Evitar pop-ups de notificações
+      const notNowButton = await page.$('button.sqdOP.yWX7d.y3zKF');
+      if (notNowButton) {
+        await notNowButton.click();
+        console.log("Botão 'Not Now' clicado.");
+      }
+
+      // Salvar cookies após o login
+      const cookies = await page.cookies();
+      fs.writeFileSync(cookiesPath, JSON.stringify(cookies, null, 2));
+      console.log("Cookies salvos.");
+
+      await page.goto(`https://www.instagram.com/${username}/`);
+      await page.waitForNavigation(); // Aguarde a navegação após o login
+    }
+console.log("Página do perfil acessível.");
+
+await delay(5000); // Aguarde o carregamento completo da página do perfil
+
+
+console.log("Viewport redefinido.");
+
+await delay(1000);
+const screenshotBase64 = await page.screenshot({ encoding: "base64" });
+capturedImages[username] = Buffer.from(screenshotBase64, "base64");
+console.log("Screenshot capturado.");
+    
     await browser.close();
     console.log(`Captura do perfil ${username} concluída.`);
+
     fs.rm(tmpDir.name, { recursive: true, force: true }, (err) => {
       if (err) {
         console.error("Erro ao remover diretório temporário:", err);
       } else {
-        console.log(`Diretório temporário ${tmpDir.name} removido com sucesso.`);
+        console.log(
+          `Diretório temporário ${tmpDir.name} removido com sucesso.`
+        );
       }
     });
 
@@ -66,7 +147,7 @@ app.get("/obter-imagem", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Running server');
+  console.log("Running server");
 });
 
 function delay(ms) {
